7a192bd8c84800c821a95977652e9b8575422a87
Yudhya Wicaksono <yudhyapatriaw@gmail.com>
2019-04-15 14:02:07 +0700

    another commit

--- app/app.ts
+++ app/app.ts
@@ -24,6 +24,7 @@ const config = {
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  console.log('dsaf');
 });
 
 app.post('/webhook', (req, res) => {
--- build/app.js
+++ build/app.js
@@ -2,10 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // lib/app.ts
 var express = require("express");
+var bot_sdk_1 = require("@line/bot-sdk");
 // Create a new express application instance
 var app = express();
+var config = {
+    channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
+    channelSecret: 'YOUR_CHANNEL_SECRET'
+};
 app.get('/', function (req, res) {
     res.send('Hello World!');
+    console.log('dsaf');
+});
+app.post('/webhook', function (req, res) {
+    res.json({});
+});
+app.post('/webhook', bot_sdk_1.middleware(config), function (req, res) {
+    req.body.events; // webhook event objects
+    req.body.destination; // user ID of the bot (optional)
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
