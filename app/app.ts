0f3793def6daa04376f66dd2f64c493e7096c337
Yudhya Wicaksono <yudhyapatriaw@gmail.com>
2019-04-15 14:06:56 +0700

    sadf

--- app/app.ts
+++ app/app.ts
@@ -24,7 +24,7 @@ const config = {
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-  console.log('dsafsd');
+  console.log('dsafsdd');
 });
 
 app.post('/webhook', (req, res) => {
