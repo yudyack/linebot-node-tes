29332649472318a1a64ee31f6d3dae5c7532baf6
Yudhya Wicaksono <yudhyapatriaw@gmail.com>
2019-04-18 14:40:16 +0700

    linux

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-# npm install -g pm2
-# npm install -g typescript
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+npm install -g pm2
+npm install -g typescript
 
 
 ## install nginx
-# sudo apt update
-# sudo apt install nginx
+sudo apt update
+sudo apt install nginx
 
 ## TODO load githooks
 cp hooks/* .git/hooks/
