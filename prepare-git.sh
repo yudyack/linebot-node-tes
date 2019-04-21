91b88c6f4941de4a2cb096fddd1817540ba40aa5
Yudhya Wicaksono <yudhyapatriaw@gmail.com>
2019-04-21 22:22:43 +0700

    sdaf

--- prepare-git.sh
+++ prepare-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+directory=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 echo "$directory"
 pwd
 cd $directory
