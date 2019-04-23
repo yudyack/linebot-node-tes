9d1197a8d8c8cbbfc4402ff21078138e0ce2567e
Yudhya Wicaksono <yudhyapatriaw@gmail.com>
2019-04-23 10:21:34 +0700

    add log for testing

--- app/app.ts
+++ app/app.ts
@@ -53,13 +53,13 @@ app.get('/', function (req, res) {
 
 app.post('/webhook', middleware(<MiddlewareConfig> config), (req, res) => {
   let events: Array<WebhookEvent> = req.body.events // webhook event objects
-  // console.log("request\n", req);
+  console.log("sending request");
 
   // req.get({url: "https://servombak.free.beeceptor.com"})
   request.post("https://servombak.free.beeceptor.com", req.body);
   // res.redirect("https://hookb.in/kx6jPqe3VKTepeoxWEXL");
 
-
+  console.log("sent?");
 
   let dest = req.body.destination // user ID of th,e bot (optional)
 
