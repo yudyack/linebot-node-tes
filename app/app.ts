3b91219738504698c38d830e75c2ac6d55343bb1
Yudhya Wicaksono <yudhyapatriaw@gmail.com>
2019-04-23 13:57:27 +0700

    local validation added

--- app/app.ts
+++ app/app.ts
@@ -45,18 +45,24 @@ import { handleEvent } from './mapEvent';
 // Create a new express application instance
 const app: express.Application = express();
 
-console.log(dataAll);
-console.log(config)
-app.get('/', function (req, res) {
+
+function onlyLocalSimple(req: express.Request, res: express.Response, next: express.NextFunction) {
+  let remote_address = req.header('remote_addr') || req.connection.remoteAddress;
+  console.log(remote_address);
+  if (remote_address == '::1') next();
+  else res.send('no');
+}
+
+app.get('/',onlyLocalSimple, function (req, res) {
   // console.log(req.connection.remoteAddress)
-  let remote_address = req.header('remote_addr');
+  let remote_address = req.header('remote_addr') || req.connection.remoteAddress;
   console.log(remote_address);
   res.send('Hello World!');
 });
 
 app.use('/webhook1',proxy("https://servombak.free.beeceptor.com"));
 
-app.post('/webhook-mock', [bodyParser.json()], (req: express.Request, res: express.Response) => {
+app.post('/webhook-mock', [bodyParser.json(), onlyLocalSimple], (req: express.Request, res: express.Response) => {
   console.log(req.hostname)
   // handle events separately
   // console.log(req);
