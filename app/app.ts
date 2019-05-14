baa5e44121fb6a9bd0e50b23b6192ad634fd8d1e
Yudhya Wicaksono <yudhyapatriaw@gmail.com>
2019-05-14 12:37:13 +0700

    sdaf

--- app/app.ts
+++ app/app.ts
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "../public/tes.html"))
 });
 
-app.use('/webhook',proxy("https://servombak.free.beeceptor.com"));
+app.use('/webhook1',proxy("https://servombak.free.beeceptor.com"));
 
 app.post('/webhook-mock', [bodyParser.json(), onlyLocalSimple], (req: express.Request, res: express.Response) => {
   console.log(req.hostname)
@@ -100,9 +100,9 @@ app.post('/webhook-mock', [bodyParser.json(), onlyLocalSimple], (req: express.Re
       console.info("all handled");
     })
   res.status(200).end()
-  });
+});
 
-app.post('/webhook1', middleware(<MiddlewareConfig> config), (req, res) => {
+app.post('/webhook', middleware(<MiddlewareConfig> config), (req, res) => {
   
   let events: Array<WebhookEvent> = req.body.events // webhook event objects
   console.log(events);
@@ -117,7 +117,10 @@ app.post('/webhook1', middleware(<MiddlewareConfig> config), (req, res) => {
 
   // handle events separately
   Promise.all(req.body.events.map(handle))
-    .then(() => {res.status(200).end()});
+    .then((i) => {
+      console.info("all handled");
+    })
+  res.status(200).end()
 })
 
 app.use('/static', express.static(path.join(__dirname,'../static')));
